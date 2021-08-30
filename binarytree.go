67417f8dd5c5ffec7e7ba5b5d3fdbe96953ba89c
wnanbei <wnanbei@qq.com>
2021-08-30 22:45:26 +0800

    feat: add binary tree count method and test

--- binarytree.go
+++ binarytree.go
@@ -2,7 +2,24 @@ package binarytree
 
 // Node is binary tree node.
 type Node struct {
-	Val   int
 	Left  *Node
 	Right *Node
+	Val   interface{}
+}
+
+// BinaryTree is binary tree
+type BinaryTree struct {
+	Root *Node
+}
+
+// Count return the count of binary tree nodes.
+func (bt *BinaryTree) Count() int64 {
+	return countBinaryTree(bt.Root)
+}
+
+func countBinaryTree(n *Node) int64 {
+	if n == nil {
+		return 0
+	}
+	return 1 + countBinaryTree(n.Left) + countBinaryTree(n.Right)
 }
--- binarytree_test.go
+++ binarytree_test.go
@@ -1,7 +1,24 @@
 package binarytree
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
-func TestBinaryTree(t *testing.T) {
+func TestBinaryTreeCount(t *testing.T) {
+	bt := new(BinaryTree)
+	bt.Root = &Node{Val: "F"}
+	bt.Root.Left = &Node{Val: "B"}
+	bt.Root.Right = &Node{Val: "G"}
 
+	bt.Root.Left.Left = &Node{Val: "A"}
+	bt.Root.Left.Right = &Node{Val: "D"}
+
+	bt.Root.Left.Right.Left = &Node{Val: "C"}
+	bt.Root.Left.Right.Right = &Node{Val: "E"}
+
+	bt.Root.Right.Right = &Node{Val: "I"}
+	bt.Root.Right.Right.Left = &Node{Val: "H"}
+
+	fmt.Println(bt.Count())
 }
